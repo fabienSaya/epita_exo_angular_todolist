0592a3bc12033c432e739a273edfe0c0d36af206
fabienSAYA <fsayatravail@hotmail.com>
2022-04-15 09:19:35 +0200

    ma version qui fonctionne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'taskManager';
+
+  title:String="Gestionnaire de tâches";
+
+  tasks:Array<any> = [
+    { title: 'Installer le CLI', done: false },
+    { title: 'Prendre un café', done: false },
+    { title: 'Créer une todo list', done: true },
+  ];
+
+
+  addTask(){
+    //https://developer.mozilla.org/fr/docs/Web/API/Window/prompt
+    let newTaskTitle = window.prompt("ajouter une tache","undefined");
+    if (newTaskTitle!=undefined) {
+      let newTask ={title: newTaskTitle, done:false};
+      //this.tasks.push(newTask);
+      this.tasks=[...this.tasks,newTask]
+    }
+  }
+
+  deleteTask(i:number) {
+    //https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    this.tasks.splice(i,1);
+  }
+
+
+
 }
